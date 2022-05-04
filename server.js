143243d2daabbccc83aa45244662621e7871c78a
Alexander Morozov <alexander.morozov@shijigroup.com>
2022-05-04 16:17:46 +0300

    implement file upload

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var cors = require('cors');
 require('dotenv').config()
+const multer  = require('multer')
+const upload = multer({ dest: 'uploads/' })
 
 var app = express();
 
@@ -9,6 +11,16 @@ app.use('/public', express.static(process.cwd() + '/public'));
 
 app.get('/', function (req, res) {
     res.sendFile(process.cwd() + '/views/index.html');
+});
+
+app.post('/api/fileanalyse', upload.single('upfile'), function(req, res) {
+  const { file } = req
+
+  res.json({
+    name: file.originalname,
+    type: file.mimetype,
+    size: file.size
+  })
 });
 
 
